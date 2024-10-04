8052bcb6c8ac49f03e258860726edaf108bf74b5
marsyhhh <marsyhhhf@gmail.com>
2024-10-05 03:38:18 +0700

    Final

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -3,29 +3,17 @@ import { auth } from "@clerk/nextjs"
 
 import prismadb from "@/lib/prismadb"
 import Navbar from "@/components/Navbar"
+import React from "react"
 
 export default async function DashboardLayout({
   children,
-  params
 }: {
   children: React.ReactNode
-  params: { storeId: string }
 }) {
   const { userId } = auth()
 
   if (!userId) {
     redirect("/sign-in")
-  }
-
-  const store = await prismadb.store.findFirst({
-    where: {
-      id: params.storeId,
-      userId
-    }
-  })
-
-  if (!store) {
-    redirect("/")
   }
 
   return (
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { UserButton, auth } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
 
-import StoreSwitcher from "@/components/store-switcher"
 import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
-import prismadb from "@/lib/prismadb"
+import { ChevronsUpDown, Store } from "lucide-react"
+import { Button } from "./ui/button"
+import { cn } from "@/lib/utils"
 
 const Navbar = async () => {
   const { userId } = auth()
@@ -13,16 +14,20 @@ const Navbar = async () => {
     redirect("/sign-in")
   }
 
-  const stores = await prismadb.store.findMany({
-    where: {
-      userId
-    }
-  })
-
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <StoreSwitcher items={stores} />
+        <Button
+          variant="outline"
+          size="sm"
+          role="combobox"
+          aria-label="Select a store"
+          className={cn("w-[200px] justify-between")}
+        >
+          <Store className="mr-2 h-4 w-4" />
+          Zhafirah Collection
+          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
+        </Button>
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <ThemeToggle />
