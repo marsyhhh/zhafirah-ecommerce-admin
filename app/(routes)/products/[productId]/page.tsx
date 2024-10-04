a8be0dbf505c0414f28d6a084bf63aa0796cd69d
marsyhhh <marsyhhhf@gmail.com>
2024-10-05 03:57:23 +0700

    Final

--- app/(routes)/products/[productId]/page.tsx
+++ app/(routes)/products/[productId]/page.tsx
@@ -1,51 +1,35 @@
-import prismadb from "@/lib/prismadb";
+import prismadb from "@/lib/prismadb"
 
-import { ProductForm } from "./components/product-form";
+import { ProductForm } from "./components/product-form"
 
-const ProductPage = async ({
-  params
-}: {
-  params: { productId: string, storeId: string }
-}) => {
+const ProductPage = async ({ params }: { params: { productId: string } }) => {
   const product = await prismadb.product.findUnique({
     where: {
       id: params.productId,
     },
     include: {
       images: true,
-    }
-  });
-
-  const categories = await prismadb.category.findMany({
-    where: {
-      storeId: params.storeId,
     },
-  });
+  })
 
-  const sizes = await prismadb.size.findMany({
-    where: {
-      storeId: params.storeId,
-    },
-  });
+  const categories = await prismadb.category.findMany()
 
-  const colors = await prismadb.color.findMany({
-    where: {
-      storeId: params.storeId,
-    },
-  });
+  const sizes = await prismadb.size.findMany()
 
-  return ( 
+  const colors = await prismadb.color.findMany()
+
+  return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <ProductForm 
-          categories={categories} 
+        <ProductForm
+          categories={categories}
           colors={colors}
           sizes={sizes}
           initialData={product}
         />
       </div>
     </div>
-  );
+  )
 }
 
-export default ProductPage;
+export default ProductPage
