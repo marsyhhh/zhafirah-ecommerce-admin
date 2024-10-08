8a9290cca1a8a13cec32deb1a4a64964c8b46dc4
marsyhhh <marsyhhhf@gmail.com>
2024-10-08 22:22:30 +0700

    Final

--- components/overview.tsx
+++ components/overview.tsx
@@ -4,11 +4,9 @@ import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
 
 interface OverviewProps {
   data: any[]
-};
+}
 
-export const Overview: React.FC<OverviewProps> = ({
-  data
-}) => {
+export const Overview: React.FC<OverviewProps> = ({ data }) => {
   return (
     <ResponsiveContainer width="100%" height={350}>
       <BarChart data={data}>
@@ -24,10 +22,10 @@ export const Overview: React.FC<OverviewProps> = ({
           fontSize={12}
           tickLine={false}
           axisLine={false}
-          tickFormatter={(value) => `$${value}`}
+          tickFormatter={(value) => `RP${value}`}
         />
         <Bar dataKey="total" fill="#3498db" radius={[4, 4, 0, 0]} />
       </BarChart>
     </ResponsiveContainer>
   )
-};
+}
